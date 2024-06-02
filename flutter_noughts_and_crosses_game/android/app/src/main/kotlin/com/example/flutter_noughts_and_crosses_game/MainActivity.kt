716a697394b9c70686988430386dad3276b2c619
kevferreira <keversonferreira_keev2@hotmail.com>
2024-06-02 12:49:33 -0300

    feat: initial project

---
+++ flutter_noughts_and_crosses_game/android/app/src/main/kotlin/com/example/flutter_noughts_and_crosses_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_noughts_and_crosses_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_noughts_and_crosses_game/lib/game_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
