0ab90b6d3e02f63d2ca3d89ccbe2376539ab6631
kevferreira <keversonferreira_keev2@hotmail.com>
2024-06-02 18:47:59 -0300

    feat: finished

--- flutter_noughts_and_crosses_game/lib/game_page.dart
+++ flutter_noughts_and_crosses_game/lib/game_page.dart
@@ -34,6 +34,7 @@ class _GamePageState extends State<GamePage> {
           children: [
             _textoCabecalho(),
             _gameContainer(),
+            _botaoReiniciar(),
           ],
         ),
       ),
@@ -88,10 +89,16 @@ class _GamePageState extends State<GamePage> {
         setState(() {
           ocupado[index] = jogadorAtual;
           trocarTurno();
+          verificarVencedor();
+          verificarEmpate();
         });
       },
       child: Container(
-        color: Colors.black26,
+        color: ocupado[index].isEmpty
+            ? Colors.black26
+            : ocupado[index] == JOGADOR_1
+                ? Colors.blue
+                : Colors.orange,
         margin: const EdgeInsets.all(8),
         child: Center(
           child: Text(
@@ -103,11 +110,79 @@ class _GamePageState extends State<GamePage> {
     );
   }
 
+  _botaoReiniciar() {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            inicializarJogo();
+          });
+        },
+        child: const Text("Reiniciar jogo"));
+  }
+
   trocarTurno() {
     if (jogadorAtual == JOGADOR_1) {
       jogadorAtual = JOGADOR_2;
     } else {
       jogadorAtual = JOGADOR_1;
     }
+  }
+
+  verificarVencedor() {
+    List<List<int>> composicoesVencedoras = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (var posicaoVencedora in composicoesVencedoras) {
+      String posicao0 = ocupado[posicaoVencedora[0]];
+      String posicao1 = ocupado[posicaoVencedora[1]];
+      String posicao2 = ocupado[posicaoVencedora[2]];
+
+      if (posicao0.isNotEmpty) {
+        if (posicao0 == posicao1 && posicao0 == posicao2) {
+          mostrarMensagemJogoEncerrado("Jogador $posicao0 venceu!");
+          jogoAcabou = true;
+          return;
+        }
+      }
+    }
+  }
+
+  verificarEmpate() {
+    if (jogoAcabou) {
+      return;
+    }
+    bool empate = true;
+    for (var lugarOcupado in ocupado) {
+      if (lugarOcupado.isEmpty) {
+        empate = false;
+      }
+    }
+
+    if (empate) {
+      mostrarMensagemJogoEncerrado("Empate!");
+      jogoAcabou = true;
+    }
+  }
+
+  mostrarMensagemJogoEncerrado(String mensagem) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          backgroundColor: Colors.green,
+          content: Text(
+            "Jogo encerrado \n $mensagem",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          )),
+    );
   }
 }
