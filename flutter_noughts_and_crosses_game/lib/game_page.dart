8264ba85b7f29f9f2b598dc4ded93108638a5068
kevferreira <keversonferreira_keev2@hotmail.com>
2024-06-02 13:48:42 -0300

    feat: included grid and states changes

--- flutter_noughts_and_crosses_game/lib/game_page.dart
+++ flutter_noughts_and_crosses_game/lib/game_page.dart
@@ -6,8 +6,108 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  static const String JOGADOR_1 = "X";
+  static const String JOGADOR_2 = "O";
+
+  late String jogadorAtual;
+  late bool jogoAcabou;
+  late List<String> ocupado;
+
+  @override
+  void initState() {
+    super.initState();
+    inicializarJogo();
+  }
+
+  void inicializarJogo() {
+    jogadorAtual = JOGADOR_1;
+    jogoAcabou = false;
+    ocupado = ["", "", "", "", "", "", "", "", ""];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _textoCabecalho(),
+            _gameContainer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textoCabecalho() {
+    return Column(
+      children: [
+        const Text(
+          "Jogo da Velha",
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          "Vez do $jogadorAtual",
+          style: const TextStyle(
+            color: Colors.black87,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _gameContainer() {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      width: MediaQuery.of(context).size.height / 2,
+      margin: const EdgeInsets.all(8),
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: 9,
+          itemBuilder: (context, int index) {
+            return _box(index);
+          }),
+    );
+  }
+
+  Widget _box(int index) {
+    return InkWell(
+      onTap: () {
+        if (jogoAcabou || ocupado[index].isNotEmpty) {
+          return;
+        }
+
+        setState(() {
+          ocupado[index] = jogadorAtual;
+          trocarTurno();
+        });
+      },
+      child: Container(
+        color: Colors.black26,
+        margin: const EdgeInsets.all(8),
+        child: Center(
+          child: Text(
+            ocupado[index],
+            style: const TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+
+  trocarTurno() {
+    if (jogadorAtual == JOGADOR_1) {
+      jogadorAtual = JOGADOR_2;
+    } else {
+      jogadorAtual = JOGADOR_1;
+    }
   }
 }
--- flutter_noughts_and_crosses_game/lib/main.dart
+++ flutter_noughts_and_crosses_game/lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jogo da Velha',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: GamePage(),
     );
